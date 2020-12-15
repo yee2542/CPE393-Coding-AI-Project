c9dd287d8965e81069794e9f0c7327d4fe284aaa
yee2542 <yee2542gtl@gmail.com>
2020-12-16 04:14:30 +0700

    Add model summary

---
+++ model.summary.py
@@ -0,0 +1,48 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam, SGD
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.utils import to_categorical
+import keras as K
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import os
+
+INIT_LR = 1e-5
+EPOCHS = 10000
+BS = 32
+baseModel = MobileNetV2(weights="imagenet", include_top=False,
+                        input_tensor=Input(shape=(224, 224, 3)))
+
+headModel = baseModel.output
+headModel = AveragePooling2D(pool_size=(4, 4))(headModel)
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(256, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
+
+model = Model(inputs=baseModel.input, outputs=headModel)
+
+for layer in baseModel.layers:
+    layer.trainable = False
+
+opt = Adam(lr=INIT_LR,
+           decay=INIT_LR / EPOCHS
+           )
+model.compile(loss="binary_crossentropy", optimizer=opt,
+              metrics=["accuracy"])
+
+model.summary()
